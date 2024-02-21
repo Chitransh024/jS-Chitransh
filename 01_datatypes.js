8ae25d25bb8a5866e8dbdd129b9da52fe92271a4
Chitransh024 <153779749+Chitransh024@users.noreply.github.com>
2024-02-21 17:01:06 +0000

    datatypes

---
+++ 01_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all js code as newer version
+//alert(2+2) // we are using nodejs, not browser
+console.log(9+2);//readability should be high
+ let name ="chitransh"
+ let age = 18
+ let isloggedin = true
+
+ //number => 2 to power 53
+ // bigint
+ // string => ""
+ // boolean => true/false
+ //null => standalone value
+ //undefines =>
+ // symbol => unique
+
+ //object 
+ console.log(typeof("chitransh"));
+ console.log(typeof null);
