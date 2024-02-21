25da1b7411ead89a49f9d075c0237b27926fa463
Chitransh024 <153779749+Chitransh024@users.noreply.github.com>
2024-02-21 17:39:47 +0000

    commit

---
+++ cndo.js
@@ -0,0 +1,23 @@
+// let score = 33;
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// let isloggedin = 1;
+// let booleaninloggedin = Boolean(isloggedin);
+// console.log(booleaninloggedin);
+// Operations
+let value = 3
+let negvalue = -value
+//console.log(negvalue)
+let str1 = "hello";
+let str2 = "chitransh";
+let str3 = str1 + str2
+console.log(str3);
+let gamecounter = 100;
+gamecounter++
+console.log(gamecounter);
+
+
+
+
