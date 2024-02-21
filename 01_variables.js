e6d747ca6d865e6bf6cb22dee10e7339a0df5116
Chitransh024 <153779749+Chitransh024@users.noreply.github.com>
2024-02-21 03:55:13 +0000

    commit

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "chitranshchaudhary2021@gmial.com"
+var accountPassword = "12345"
+accountCity = "Mathura"
+
+// accountId = 2 // not allowed
+accountEmail = "cc@.com"
+accountPassword = "21232344"
+accountCity = "Bangalore"
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountId, accountEmail, accountPassword,accountCity])
